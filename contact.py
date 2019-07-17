a282afe2c1d37f27a311ae5b78637102bab59eec
Janice-M <janiceink001@gmail.com>
2019-07-17 15:45:10 +0300

    ran a test

--- contact.py
+++ contact.py
@@ -1,17 +1,15 @@
 class Contact:
     """
-    Class that generates new instances of contacts
+    Class that generates new instances of contacts.
     """
 
-    pass
-def __init__(self,first_name,last_name,phone_number,email):
+    contact_list = []  # Empty contact list
 
-        '''
-        __init__ method that helps us define properties for our objects.
+    def __init__(self, first_name, last_name, number, email):
 
-        Args:
-            first_name: New contact first name.
-            last_name : New contact last name.
-            number: New contact phone number.
-            email : New contact email address.
-        '''
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
